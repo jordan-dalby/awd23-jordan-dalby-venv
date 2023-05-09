25c4dfd9e56f954ce8bd7d57fd655136fc0e4a26
Jordan <jordan.dalby@yorksj.ac.uk>
2023-05-09 14:08:56 +0100

    removed all start scripts due to some bugs

--- flask-server/startServer-ysjcs.sh
+++
@@ -1,8 +0,0 @@
-chmod 777 ./Scripts/activate
-chmod 777 ./Scripts/deactivate
-
-./Scripts/activate
-cd flask-server
-/usr/bin/python3 restServer-mvc.py
-cd ..
-./Scripts/deactivate
--- startServer-ysjcs.sh
+++
@@ -1,5 +0,0 @@
-chmod 777 ./Scripts/activate
-
-./Scripts/activate
-cd flask-server
-/usr/bin/python3 restServer-mvc.py
